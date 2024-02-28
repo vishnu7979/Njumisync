27427515bcc05f49c3c741c5da1d2cd95b97ea0a
vishnu <vishnushaji797@gmail.com>
2024-02-28 21:24:57 +0530

    firstest  commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import db from "@/lib/superbase/db";
+import { ThemeProvider } from "@/lib/providers/next-theme-provider";
+import {DM_Sans} from 'next/font/google';
+import { twMerge } from "tailwind-merge";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,9 +18,19 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  console.log(db);
+  
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+        <body className={twMerge('bg-background', inter.className)}>
+        <ThemeProvider
+         attribute="class"
+         defaultTheme="dark"
+         enableSystem
+        >
+        {children}
+        </ThemeProvider>
+        </body>
     </html>
   );
 }
