6f38a00473aca2cdb0c82ebd7a88f01d818e5b10
vishnu <vishnushaji797@gmail.com>
2024-02-28 23:01:37 +0530

    after header section

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,10 +1,11 @@
 // import Header from '@/components/landing-page/header';
+import Header from '@/components/landing-page/header';
 import React from 'react';
 
 const HomePageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
-      {/* <Header /> */}
+      <Header />
       {children}
     </main>
   );
